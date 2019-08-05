f8e6259468fd749fcb142f480d198a611fa1ab4a
Roso Peñaranda <rosopenaranda@gmail.com>
2019-08-05 17:03:03 -0500

    finalizados primeros 2 modulos del curso

---
+++ platzigram/views.py
@@ -0,0 +1,25 @@
+#Django
+from django.http import HttpResponse
+
+
+#utilities
+from datetime import datetime
+
+def helloWorld(request):
+    return HttpResponse('<html><head><body><h1>Hello World!</h1></body></head></html>')
+
+
+def time(request):
+    
+    now = datetime.now().strftime('%b %d, %Y - %H:%M')
+    return HttpResponse('hora: {now}'.format(now=str(now)))
+
+
+def hi(request):
+  from django.http import JsonResponse
+  #import pdb;pdb.set_trace()
+  numbers = [int(i) for i in request.GET['numbers'].split(',')]
+  OrderNumbers = sorted(numbers)
+  
+  print(OrderNumbers)
+  return JsonResponse({'numbers': OrderNumbers})
