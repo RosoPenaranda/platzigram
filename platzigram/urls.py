05eee5e7898601f428e3828b2ee8eeb04a91aff1
Roso Peñaranda <rosopenaranda@gmail.com>
2019-09-10 16:44:26 -0500

    cap 19

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,20 +1,9 @@
-"""platzigram URL Configuration
+''' URLs module'''
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # Django
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from platzigram import views as local_views
@@ -28,4 +17,4 @@ urlpatterns = [
     path('hi/',local_views.hi),
     path('posts/', posts_views.list_posts),
     path("conf/qrconf.php", local_views.helloWorld)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
