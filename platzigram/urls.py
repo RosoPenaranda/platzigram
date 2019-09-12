a0d6a931d494cceae7ca4d9dcd0e931aed3e4f8a
Roso Peñaranda <rosopenaranda@gmail.com>
2019-09-12 16:34:23 -0500

    clase 24 terminada

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -8,6 +8,8 @@ from django.contrib import admin
 
 from platzigram import views as local_views
 from posts import views as posts_views
+from users import views as users_views
+ 
 
 
 urlpatterns = [
@@ -15,6 +17,13 @@ urlpatterns = [
     path('hola/', local_views.helloWorld),
     path('hora/', local_views.time),
     path('hi/',local_views.hi),
-    path('posts/', posts_views.list_posts),
-    path("conf/qrconf.php", local_views.helloWorld)
+    path('posts/', posts_views.list_posts, name='feed'),
+    path("conf/qrconf.php", local_views.helloWorld),
+    path('users/login/',users_views.login_view, name='login'),
+    path('users/logout/',users_views.logout_view, name='logout'),
+    path('users/signup/',users_views.signup, name='signup'),
+    path('users/me/profile/',users_views.update_profile, name='update_profile'),
+
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 # Django
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # utilitis
 from datetime import datetime
@@ -36,6 +37,6 @@ posts = [
     }
 ]
 
-
+@login_required
 def list_posts(request):
-  return render(request, 'feed.html', {'posts': posts})
+  return render(request, 'posts/feed.html', {'posts': posts})
